f1b4943edfea780c24e8bd4264c206ac60772958
jasmine0714 <woooooooow22@gmail.com>
2024-04-29 20:04:39 +0900

    🧑‍💻 decorator 학습

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() body: any,
+    @Param('id') id: string,
+    @Param('name') name: string,
+  ): string {
+    console.log(id);
+    console.log(name);
     return this.appService.getHello();
   }
 }
