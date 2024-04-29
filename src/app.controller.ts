c9e05b89dde8f2928af799ae58af6172c86a9689
jasmine0714 <woooooooow22@gmail.com>
2024-04-29 21:46:19 +0900

    ✨ app.controller.ts에서 고양이 리스트 응답

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
+import { Cat, CatsService } from './cats/cats.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Cat[] {
+    return this.catsService.getCats();
   }
 }
