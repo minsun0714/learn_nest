9b3b82880aefeadf2ca0aea7a0bd36d4ece08c54
jasmine0714 <woooooooow22@gmail.com>
2024-04-29 21:18:59 +0900

    ✨ cats CRUD 구현

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Cat, CatsService } from './cats.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly CatsService: CatsService) {}
+
+  @Get()
+  @HttpCode(200)
+  getCats(): Cat[] {
+    return this.CatsService.getCats();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  getCatById(@Param('id') id: string): Cat | undefined {
+    console.log('📢[cats.controller.ts:15]: id: ', id);
+    return this.CatsService.getCatById(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  createCatById(@Body() body: { name: string }) {
+    this.CatsService.createCatById(body);
+    return '고양이를 성공적으로 추가했습니다.';
+  }
+
+  @Put(':id')
+  @HttpCode(201)
+  updateCatById(@Param('id') id: string, @Body() body: { name: string }) {
+    this.CatsService.updateCatById(id, body);
+    return '고양이의 이름을 성공적으로 수정했습니다.';
+  }
+
+  @Delete(':id')
+  @HttpCode(201)
+  deleteCatById(@Param('id') id: string) {
+    this.CatsService.deleteCatById(id);
+    return '고양이를 성공적으로 삭제했습니다.';
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
+export type Cat = {
+  id: string;
+  name: string;
+};
+
+let cats = [
+  { id: '0', name: '집돌이' },
+  { id: '1', name: '집순이' },
+];
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  getCats(): Cat[] {
+    return cats;
+  }
+
+  getCatById(id: string): Cat | undefined {
+    return cats.find((cat: Cat) => cat.id === id);
+  }
+
+  createCatById(body: { name: string }) {
+    const newId = String(Number(cats[cats.length - 1].id) + 1);
+    cats.push({
+      id: newId,
+      name: body.name,
+    });
+  }
+
+  updateCatById(id: string, body: { name: string }) {
+    let oldCat = cats.find((cat: Cat) => cat.id === id);
+    console.log('📢[cats.service.ts:32]: oldCat: ', cats);
+    if (oldCat) {
+      oldCat.name = body.name;
+    }
+  }
+
+  deleteCatById(id: string) {
+    cats = cats.filter((cat) => cat.id !== id);
+  }
+}
