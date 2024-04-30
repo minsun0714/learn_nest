12ce48452008d6c956430521533f5aa196ea0b29
jasmine0714 <woooooooow22@gmail.com>
2024-04-30 14:30:14 +0900

    ✨ Controller 단위 예외 처리

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,3 +1,4 @@
+import { HttpExceptionFilter } from './../http-exception.filter';
 import { Cat, CatsService } from './cats.service';
 import {
   Body,
@@ -10,46 +11,78 @@ import {
   Param,
   Post,
   Put,
+  UseFilters,
 } from '@nestjs/common';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
 
   @Get()
-  @HttpCode(200)
-  getCats(): Cat[] {
-    throw new HttpException(
-      { status: HttpStatus.INTERNAL_SERVER_ERROR, error: 'API is broken' },
-      HttpStatus.INTERNAL_SERVER_ERROR,
-    );
+  getCats(): Cat[] | void {
+    try {
+      this.CatsService.getCats();
+    } catch (error) {
+      throw new HttpException(
+        'Api is broken',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get(':id')
   @HttpCode(200)
-  getCatById(@Param('id') id: string): Cat | undefined {
-    console.log('📢[cats.controller.ts:15]: id: ', id);
-    return this.CatsService.getCatById(id);
+  getCatById(@Param('id') id: string): Cat | void {
+    try {
+      this.CatsService.getCatById(id);
+    } catch (error) {
+      throw new HttpException(
+        'Api is broken',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Post()
   @HttpCode(201)
   createCatById(@Body() body: { name: string }) {
-    this.CatsService.createCatById(body);
-    return '고양이를 성공적으로 추가했습니다.';
+    try {
+      this.CatsService.createCatById(body);
+      return '고양이를 성공적으로 추가했습니다.';
+    } catch (error) {
+      throw new HttpException(
+        'Api is broken',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Put(':id')
   @HttpCode(201)
   updateCatById(@Param('id') id: string, @Body() body: { name: string }) {
-    this.CatsService.updateCatById(id, body);
-    return '고양이의 이름을 성공적으로 수정했습니다.';
+    try {
+      this.CatsService.updateCatById(id, body);
+      return '고양이의 이름을 성공적으로 수정했습니다.';
+    } catch (error) {
+      throw new HttpException(
+        'Api is broken',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Delete(':id')
   @HttpCode(201)
   deleteCatById(@Param('id') id: string) {
-    this.CatsService.deleteCatById(id);
-    return '고양이를 성공적으로 삭제했습니다.';
+    try {
+      this.CatsService.deleteCatById(id);
+      return '고양이를 성공적으로 삭제했습니다.';
+    } catch (error) {
+      throw new HttpException(
+        'Api is broken',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -13,9 +13,11 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const errorMsg = exception.message;
 
     response.status(status).json({
-      statusCode: status,
+      error: errorMsg,
+      success: false,
       timestamp: new Date().toISOString(),
       path: request.url,
     });
