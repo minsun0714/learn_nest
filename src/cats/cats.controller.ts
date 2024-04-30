7afb9ecf8f71bc9e088c27a56c3854394ae343ed
jasmine0714 <woooooooow22@gmail.com>
2024-04-30 17:44:11 +0900

    🔥 새로운 API 생성할 예정이므로 이전 API 삭제

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -25,74 +25,24 @@ export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
 
   @Get()
-  getCats(): Cat[] | void {
-    try {
-      return this.CatsService.getCats();
-    } catch (error) {
-      throw new HttpException(
-        'Api is broken',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  @Get(':id')
-  @HttpCode(200)
-  getCatById(
-    @Param('id', ParseIntPipe, PositiveIntegerPipe) id: number,
-  ): Cat | void {
-    try {
-      return this.CatsService.getCatById(id);
-    } catch (error) {
-      throw new HttpException(
-        'Api is broken',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  getCurrentCat() {
+    return 'current cat';
   }
 
   @Post()
-  @HttpCode(201)
-  createCatById(@Body() body: { name: string }) {
-    try {
-      this.CatsService.createCatById(body);
-      return '고양이를 성공적으로 추가했습니다.';
-    } catch (error) {
-      throw new HttpException(
-        'Api is broken',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async signUp() {
+    return 'signUp';
   }
-
-  @Put(':id')
-  @HttpCode(201)
-  updateCatById(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() body: { name: string },
-  ) {
-    try {
-      this.CatsService.updateCatById(id, body);
-      return '고양이의 이름을 성공적으로 수정했습니다.';
-    } catch (error) {
-      throw new HttpException(
-        'Api is broken',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  @Post('login')
+  login() {
+    return 'login';
   }
-
-  @Delete(':id')
-  @HttpCode(201)
-  deleteCatById(@Param('id', ParseIntPipe) id: number) {
-    try {
-      this.CatsService.deleteCatById(id);
-      return '고양이를 성공적으로 삭제했습니다.';
-    } catch (error) {
-      throw new HttpException(
-        'Api is broken',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  @Post('logout')
+  logout() {
+    return 'logout';
+  }
+  @Post()
+  uploadCatImg() {
+    return 'uploadImg';
   }
 }
