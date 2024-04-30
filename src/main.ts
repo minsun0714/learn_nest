dec24a3fa1bf32b8d365e12ef923d7ede8ff6434
jasmine0714 <woooooooow22@gmail.com>
2024-04-30 17:53:21 +0900

    ✨ validationPipe를 전역에 적용

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = Number(process.env.PORT);
-  console.log('📢[main.ts:9]: PORT: ', PORT);
   await app.listen(PORT);
 }
 bootstrap();
